05d865f6ad51937432df4348ae274cd5bb1a0a7f
Kakawka1234 <nazarsobcuk@gmail.com>
2025-05-29 23:55:09 +0300

    Added site

--- clinicbd/views.py
+++ clinicbd/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Appointment
 
-# Create your views here.
+def about(request):
+    return render(request, 'base.html')
+
+def appointments(request):
+    all_appointments = Appointment.objects.select_related('animal').all()
+    return render(request, 'app.html', {
+        'appointments': all_appointments
+    })
